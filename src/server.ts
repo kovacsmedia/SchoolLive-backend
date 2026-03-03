a7bfd1364a5e686d3c34dd2f333c149742636101
kovacsmedia <balazs@kovacsmedia.hu>
2026-03-03 23:01:23 +0100

    User add and modify

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import "dotenv/config";   // <-- EZ HIÁNYZOTT
 
 import { app } from "./app";
 import { env } from "./config/env";
-
+import usersAdminRoutes from "./modules/users/users.admin.routes";
+app.use("/admin/users", usersAdminRoutes);
 app.listen(env.PORT, () => {
   console.log(`API listening on port ${env.PORT}`);
 });
