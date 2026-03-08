0b5cffc4d858b2134ccb6e2326eac36abfda158f
kovacsmedia <balazs@kovacsmedia.hu>
2026-03-08 22:33:19 +0100

    Fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import "dotenv/config";   // <-- EZ HIÁNYZOTT
-import radioRoutes from "./modules/radio/radio.routes";
-import { startRadioScheduler } from "./modules/radio/radio.scheduler";
-
-
+import "dotenv/config";
 import { app } from "./app";
 import { env } from "./config/env";
 import usersAdminRoutes from "./modules/users/users.admin.routes";
+import radioRoutes from "./modules/radio/radio.routes";
+import { startRadioScheduler } from "./modules/radio/radio.scheduler";
+
 app.use("/admin/users", usersAdminRoutes);
-app.listen(env.PORT, () => {
-console.log(`API listening on port ${env.PORT}`);
 app.use("/radio", radioRoutes);
+
 startRadioScheduler();
+
+app.listen(env.PORT, () => {
+  console.log(`API listening on port ${env.PORT}`);
 });
