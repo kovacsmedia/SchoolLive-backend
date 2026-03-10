9b538da1d5421aa815cc498eb13c0164d26484d0
kovacsmedia <balazs@kovacsmedia.hu>
2026-03-10 15:31:53 +0100

    fixes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
-import "dotenv/config";   // <-- EZ HIÁNYZOTT
+import "dotenv/config";
 
 import { app } from "./app";
 import { env } from "./config/env";
-import { startBellsScheduler } from "./modules/bells/bells.scheduler";
+import { startBellsScheduler } from "./modules/bells/bell.scheduler";
+import { startRadioScheduler } from "./modules/radio/radio.scheduler";
 import usersAdminRoutes from "./modules/users/users.admin.routes";
+
 startBellsScheduler();
+startRadioScheduler();
+
 app.use("/admin/users", usersAdminRoutes);
 app.listen(env.PORT, () => {
   console.log(`API listening on port ${env.PORT}`);
